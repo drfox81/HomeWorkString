0eae063c2ee7cc4571f1471f296cc6ff2e0f5238
Drfox81 <Drfox11181@mail.ru>
2022-10-19 19:25:07 +0300

    Шеф, все готово!

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Задание 1-1");
+        System.out.println("ФИО сотрудника — " + task1());
+        System.out.println();
+
+        System.out.println("Задание 1-2");
+        task2();
+        System.out.println();
+
+        System.out.println("Задание 1-3");
+        task3();
+        System.out.println();
+
+
+    }
+
+    private static void task3() {
+        String fullName = "Иванов Семён Семёнович";
+        String nameCheck = fullName.replace("ё", "е");
+        System.out.println(nameCheck);
+    }
+
+    private static void task2() {
+        String nameUp = task1().toUpperCase();
+        System.out.println(nameUp);
+    }
+
+    public static String task1() {
+        String firstName = "Ivanov";
+        String middleName = "Ivan";
+        String lastName = "Ivanovich";
+        String fullName = firstName + " " + middleName + " " + lastName;
+        return fullName;
     }
 }
